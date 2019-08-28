9404a40ccc8531890c47e86b8d16fa9ab45b6e5e
Don Smith <don@devacademy.co.nz>
2019-08-28 20:32:34 +1200

    fix misspelling

--- tests/client/config/enzyme.js
+++ tests/client/config/enzyme.js
@@ -4,6 +4,6 @@ import Adapter from 'enzyme-adapter-react-16'
 configure({ adapter: new Adapter() })
 
 // https://dev.to/letsbsocial1/requestanimationframe--polyfill-in-react-16-2ce
-global.requestAnimationFrme = callback => {
+global.requestAnimationFrame = callback => {
   setTimeout(callback, 0)
 }
